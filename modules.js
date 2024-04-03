fc50000442eca6a66bd87ddac1ce32b11eb57af9
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-03 20:01:07 +1100

    all file system

--- modules.js
+++ modules.js
@@ -1,2 +1,18 @@
-const result = require('./test')
-console.log(result.people, result.ages)
+const fs = require('fs');
+const { f } = require('vue-carousel-3d');
+// fs.readFile('./d', (err,data)=>{
+    // if(err){
+        // console.log(err)
+    // }
+    // console.log(data.toString())
+// })
+// 
+// fs.appendFile('./blog1.txt','\nhello world', ()=>{
+    // console.log('salam chetori')
+// })
+fs.mkdir('./opencode',()=>{
+
+})
+fs.rmdir('./opencode',()=>{})
+
+fs.unlink('./opencode',()=>{})
