2567a125cc67eafe53eed85c64de0644be6613ed
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-05 21:30:22 +1100

    add res and req

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const http= require('http')
-const server= http.createServer((req,res)=>{
-    console.log('server is running')
+
+const server =http.createServer((req, res) =>{
+    console.log(req.url ,req.method )
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hello World</h1>')
+    res.end()
 })
-server.listen(3000,'local host',()=>{
-    console.log('server is running on port 3000')
+server.listen(3000,'localhost',()=>{
+    console.log('server is running')
 })
