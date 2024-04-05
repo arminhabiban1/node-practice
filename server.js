396fe6707d4df930edce6fd54edc3dca2dadccc9
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-05 21:53:25 +1100

    using res with fs

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http= require('http')
+const fs = require('fs')
 
 const server =http.createServer((req, res) =>{
     console.log(req.url ,req.method )
     res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>Hello World</h1>')
-    res.end()
+    fs.readFile('./index.html',(err, data) =>{
+        if(err) {console.log(err)}
+        else{
+            res.write(data)
+            res.end()
+        }
+    })
+   
+   
 })
 server.listen(3000,'localhost',()=>{
     console.log('server is running')
