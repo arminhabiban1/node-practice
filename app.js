0b58a31ed390da16894c8c219f1fed5ca512f187
arminhabiban1 <132064497+arminhabiban1@users.noreply.github.com>
2024-04-09 15:48:28 +1000

    margan middwear and static file

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // express app
 
 const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(morgan('dev'))
 
 app.listen(3000)
-
+l
 app.get('/',(req,res)=>{
+res.render('index' ,{title:'home'})
+    
+})
+app.get('/about',(req,res)=>{
+    res.sendFile('./view/about.html',{root: __dirname})
 
-    res.send('Hello World')
+})
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about')
+
+})
+app.use((req,res)=>{
+
+    res.sendFile('./view/404.html',{root: __dirname})
+
 })
